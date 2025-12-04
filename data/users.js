9e9a79eb6a8a1c4c26433d5dfe4afc78940d59a0
user <user@DESKTOP-H2LMAI21>
2025-12-04 14:54:46 -0500

    Removed register route

--- data/users.js
+++ data/users.js
@@ -198,58 +198,6 @@ export const acceptInvite = async (id, password) => {
   }
 }
 
-
-export const register = async (
-  companyName,
-  email,
-  password,
-  role,
-) => {
-  if (!companyName || !email || !password || !role)
-    throw "All fields need to have valid values";
-
-  companyName = checkName(companyName);
-  if (!checkEmail(email))
-    throw 'Email is invalid';
-
-  email = email.trim().toLowerCase();
-
-  const client = await pool.connect();
-  try {
-    const dupCheck = await client.query(
-      `SELECT email FROM adminportal WHERE email = $1`,
-      [email]
-    );
-
-    if (dupCheck.rowCount > 0)
-      throw `User with id of ${email} already exists`;
-
-    password = checkPassword(password);
-    const hashedPassword = await bcrypt.hash(password, 16);
-
-    role = checkRole(role);
-
-    const signupDate = getSignupDate();
-    const lastLogin = null;
-
-    const insertQuery = `
-      INSERT INTO adminportal (companyname, email, "password", "role", signupdate, lastlogin)
-      VALUES ($1, $2, $3, $4, $5, $6)
-      RETURNING email
-    `;
-    const insertValues = [companyName, email, hashedPassword, role, signupDate, lastLogin];
-
-    const insertInfo = await client.query(insertQuery, insertValues);
-
-    if (!insertInfo.rowCount || !insertInfo.rows[0])
-      throw `Could not add user with ID of ${email}`;
-
-    return { registrationCompleted: true };
-  } finally {
-    client.release();
-  }
-};
-
 export const login = async (email, password) => {
   if(email == adminEmail && password == adminPassword){
       const returnedObj = {
--- routes/auth_routes.js
+++ routes/auth_routes.js
@@ -45,57 +45,6 @@ router
   });
 
 router
-  .route('/register')
-  .post(async (req, res) => {
-    try {
-      let companyName = req.body.companyName;
-      let email = req.body.email;
-      let password = req.body.password;
-      let confirmPassword = req.body.confirmPassword;
-      let role = req.body.role;
-
-      let missing = []
-      if (!companyName)
-        missing.push("companyName")
-      if (!email)
-        missing.push("email")
-      if (!password)
-        missing.push("password")
-      if (!confirmPassword)
-        missing.push("confirmPassword")
-      if (!role)
-        missing.push("role")
-
-      if (missing.length > 0){
-        return res.status(400).render('register', {
-          error: `The missing fields are: ${missing.join(", ")}`
-        })
-      }
-
-      companyName = checkName(companyName)
-      password = checkPassword(password)
-      confirmPassword = checkPassword(confirmPassword)
-
-      if (password !== confirmPassword){
-        return res.status(400).json({ error: "The passwords do not match" });
-      }
-
-      role = checkRole(role)
-
-      const registered = await register(companyName, email, password, role)
-      if (registered.registrationCompleted){
-        return res.status(200).json( { message: "Successfully signed up" } );
-      } else {
-        return res.status(500).json({ error: "Internal Server Error" });
-      }
-    } catch (e) {
-      console.log(e)
-      return res.status(400).json({ error: e }) 
-    }
-     
-  });
-
-  router
   .route('/register/:id')
   .post(async (req, res) => {
     try {
@@ -165,7 +114,7 @@ router.route('/invite').post(async (req, res) => {
 router.route('/signout').get(async (req, res) => {
   try {
     req.session.destroy()
-    res.status(200).json("Successfully signed out")
+    res.status(200).json( { message: "Successfully signed out" } )
   } catch (e) {
     return res.status(500).json({ error: 'Internal Server Error' })
   }
